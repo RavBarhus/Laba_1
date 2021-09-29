7f5b141aa2f3fc28ae8bb74376f0b9f428879a94
RavBarhus <lisforeverdead@gmail.com>
2021-09-29 22:25:58 +0300

    Палиндром

--- src/com/company/Palindrome.java
+++ src/com/company/Palindrome.java
@@ -7,7 +7,7 @@ public class Palindrome {
             System.out.println(s + " " + isPalindrome(s));
         }
     }
-    public static String reverseString(String str) {
+    public static String reverseString(String str) { // Переворот строки
         char[] array = str.toCharArray();
         String result = "";
         for (int i = array.length - 1; i >= 0; i--) {
@@ -16,8 +16,7 @@ public class Palindrome {
         return result;
     }
 
-
-    public static boolean isPalindrome(String s){//Сравнение с предыдущим
+    public static boolean isPalindrome(String s){ // Сравнение с предыдущим значением
         String s1 = reverseString(s);
         return s1.equals(s);
     }
