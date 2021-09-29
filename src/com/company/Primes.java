dedfe75d9a2720bb586583edf1814a998ff360fa
RavBarhus <lisforeverdead@gmail.com>
2021-09-29 20:53:28 +0300

    234

---
+++ src/com/company/Primes.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Primes {
+    public static void main( String[] args) {
+        for (int i = 1; i != 100; i++) { // Цикл, в котором прогоняем числа в заданном диапазоне
+            if (isPrime(i) == true) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) { // Основной цикл
+                return false;
+            }
+        }
+        return true;
+    }
+}
