f5cb75182d0dcb101e4b56885044ffdecd469850
RavBarhus <lisforeverdead@gmail.com>
2021-09-29 22:22:42 +0300

    Ядро

---
+++ src/com/company/Palindrome.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];
+            System.out.println(s + " " + isPalindrome(s));
+        }
+    }
+    public static String reverseString(String str) {
+        char[] array = str.toCharArray();
+        String result = "";
+        for (int i = array.length - 1; i >= 0; i--) {
+            result = result + array[i];
+        }
+        return result;
+    }
+
+
+    public static boolean isPalindrome(String s){//Сравнение с предыдущим
+        String s1 = reverseString(s);
+        return s1.equals(s);
+    }
+}
